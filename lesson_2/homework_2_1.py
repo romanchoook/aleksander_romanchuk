df55d6bac08e3c28f14ea3dd2774afa3ba05b1c7
Alexander <romanchoook@gmail.com>
2025-10-07 19:29:07 +0300

    homework 2_1

---
+++ lesson_2/homework_2_1.py
@@ -0,0 +1,53 @@
+print("Числа и арифметические операции")
+###1
+print("Задание 1:")
+a, b, x, y = 17, 11, 3.2, -1.3
+print (a, type(a), b, type(b), x, type(x), y, type(y))
+
+###2
+print("Задание 2:")
+num1 = 13
+num2 = 21
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num1 ** num2)
+
+###3
+print("Задание 3:")
+a = 10
+b = 3
+print(a / b)
+print(a // b)
+print(a % b)
+a = -10
+b = 3
+print(a / b)
+print(a // b)
+print(a % b)
+a = 10
+b = -3
+print(a / b)
+print(a // b)
+print(a % b)
+
+###4
+print("Задание 4:")
+print(5 + 3 * 2)
+print((5 + 3) * 2)
+print(10 / 2 ** 2)
+
+###5
+print("Задание 5:")
+count = 10
+count += 5
+count -= 3
+count *= 2
+count /= 4
+print(count)
+
+
+
