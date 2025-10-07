51df0b394ebdecd2fa598cce148f08730d89ab44
Alexander <romanchoook@gmail.com>
2025-10-07 11:48:54 +0300

    lesson 1

--- lesson_1/lesson_1_1.py
+++ lesson_1/lesson_1_1.py
@@ -1,1 +1,31 @@
-print ("Hello world")
+# str_1 = "строка"
+# a = 7
+# b = 8.34
+# a = 8
+# print (b)
+
+
+# a = b = c = 0
+# a, b, c = 0, 0, 0
+
+
+# a, b = 8, 10
+# a, b = b, a
+# print (a)
+# print (b)
+
+# print (type(a))
+# print (type(str_1))
+# print (type(b))
+
+# message = "Здарова"
+# count = 0
+# Count = 0
+
+a = c = 7
+b, d = "строка", "строка"
+
+print(id(a))
+print(id(b))
+print(id(c))
+print(id(d))
