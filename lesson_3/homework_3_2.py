714321042df9c0b83ed9703024d203696dae7b96
Alexander <romanchoook@gmail.com>
2025-10-08 20:32:45 +0300

    delete comments on 3_2

--- lesson_3/homework_3_2.py
+++ lesson_3/homework_3_2.py
@@ -1,55 +1,55 @@
-# ###1
-# print("Задание 1:")
-#
-# print(5 > 3)
-# print(10 < 2)
-# print(7 == 7)
-# print(6 != 8)
-# print(4 >= 4)
-# print(9 <= 3)
-#
-# res = 8 > 12
-# print(res, type(res))
-#
-#
-# ###2
-# print("Задание 2:")
-# x = 15
-# print(x % 2 == 0)
-# print(x % 5 == 0)
-# print(x % 5 == 0)
-# print(x % 3 == 0 and x % 5 == 0)
+###1
+print("Задание 1:")
 
-# ###3
-# print("Задание 3:")
-# y = 4.5
-# print(y >= 1 and y <= 10)
-# print(0 <= y <= 5 or 10 <= y <= 15)
-# print(not (y < 5))
+print(5 > 3)
+print(10 < 2)
+print(7 == 7)
+print(6 != 8)
+print(4 >= 4)
+print(9 <= 3)
 
-# ###4
-# print("Задание 4:")
-# print(True or False and False)
-# print(not False and True)
-# print(False or not True and True)
-# print(not (10 > 5 or 3 < 1))
+res = 8 > 12
+print(res, type(res))
 
-# ###5
-# print("Задание 5:")
-#
-# print(bool(0))
-# print(bool(-5))
-# print(bool(3.14))
-# print(bool(""))
-# print(bool("Python"))
-# print(bool(" "))
 
-# ###6
-# print("Задание 6:")
-#
-# n = int(input("Введите n: "))
-#
-# print(f"{n} - положительное число? -", n > 0)
-# print(f"{n} - четное число? -", n % 2 == 0)
-# print(f"{n} - делится на 3? -", n % 3 == 0)
+###2
+print("Задание 2:")
+x = 15
+print(x % 2 == 0)
+print(x % 5 == 0)
+print(x % 5 == 0)
+print(x % 3 == 0 and x % 5 == 0)
+
+###3
+print("Задание 3:")
+y = 4.5
+print(y >= 1 and y <= 10)
+print(0 <= y <= 5 or 10 <= y <= 15)
+print(not (y < 5))
+
+###4
+print("Задание 4:")
+print(True or False and False)
+print(not False and True)
+print(False or not True and True)
+print(not (10 > 5 or 3 < 1))
+
+###5
+print("Задание 5:")
+
+print(bool(0))
+print(bool(-5))
+print(bool(3.14))
+print(bool(""))
+print(bool("Python"))
+print(bool(" "))
+
+###6
+print("Задание 6:")
+
+n = int(input("Введите n: "))
+
+print(f"{n} - положительное число? -", n > 0)
+print(f"{n} - четное число? -", n % 2 == 0)
+print(f"{n} - делится на 3? -", n % 3 == 0)
 
