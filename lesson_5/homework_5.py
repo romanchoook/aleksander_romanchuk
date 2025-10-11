eb5cac04e6d12dbf5b75332806621ce73d161ec9
Alexander <romanchoook@gmail.com>
2025-10-11 21:52:50 +0300

    complete all tasks homework_5

--- lesson_5/homework_5.py
+++ lesson_5/homework_5.py
@@ -50,4 +50,63 @@ cities1[1:3] = ["Сочи", "Нижний Новгород"]
 cities1[1::2] = ["Город"] * len(cities1[1::2])
 cities1[1:3] = "Волгоград", "Омск"
 
-#остановился на 5. Операции с объединением списков
+print([1, 2, 4] + [4, 5, 6])
+print(["Python", "rocks"] * 2)
+
+print([1, 2, 3] == [1, 2, 3])
+print([10, 5, 3] > [5, 10, 3])
+#print([1, 2, 3] > [1, 2, "abc"]) - возвращается ошибка '>' not supported between instances of 'int' and 'str' - нельзя сравнить строку с целым числом
+
+chars = list("Python")
+print(max(chars))
+print(min(chars))
+#print(sum(chars[:])) - unsupported operand type(s) for +: 'int' and 'str'. не получится сложить строковые составляющие, их можно либо объединить, либо преобразовать в числа и только тогда сложить
+
+
+#Методы списков
+numbers = [5, 10, 15]
+numbers.append(20)
+numbers.insert(1, 7)
+numbers.append("Python")
+
+numbers.remove(10)
+print(numbers.pop(-1))
+numbers.pop(1)
+numbers.clear()
+
+letters = ["a", "b", "c"]
+letters_1 = letters.copy()
+letters_2 = list(letters)
+print(f"копия и оригинальный список - разные объекты? - {id(letters_1) != id(letters_2) != id(letters)}")
+
+marks = [2, 3, 5, 3, 4, 5, 2, 3]
+print(marks.count(3))
+print(6 in marks)
+print(marks.index(5))
+
+
+nums = [8, 2, 5, 1, 7]
+nums.sort()
+nums.sort(reverse=True)
+nums.reverse()
+
+cities = ["Москва", "Якутск", "Санкт-Петербург"]
+cities.sort()
+sort_cities = sorted(cities) # - sort изменяет исходный список, получается что новый список по содержанию равен исходному
+
+chars = list("programming")
+print(chars.count("g"))
+chars.reverse()
+chars.sort() #- список отсортируется в алфавитном порядке
+
+
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
+]
+print(matrix)
+print(matrix[1])
+print(matrix[2][0])
+matrix[0] = len(matrix[0]) * [0]
+matrix[1][-1] = "Python"
